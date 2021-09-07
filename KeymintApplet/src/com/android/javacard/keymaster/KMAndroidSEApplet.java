eda8776ed100ccb6da4aec021b706f2d3a3ca0fc
AvinashHedage <avinash.hedage@quest-global.com>
2021-09-07 16:03:29 +0530

    keymit jcop code backup

---
+++ KeymintApplet/src/com/android/javacard/keymaster/KMAndroidSEApplet.java
@@ -0,0 +1,395 @@
+/*
+ * Copyright(C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" (short)0IS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.javacard.keymaster;
+
+import org.globalplatform.upgrade.Element;
+import org.globalplatform.upgrade.OnUpgradeListener;
+import org.globalplatform.upgrade.UpgradeManager;
+
+import javacard.framework.APDU;
+import javacard.framework.ISO7816;
+import javacard.framework.ISOException;
+import javacard.framework.Util;
+
+public class KMAndroidSEApplet extends KMKeymasterApplet implements OnUpgradeListener {
+	
+  private static final byte KM_BEGIN_STATE = 0x00;
+  private static final byte ILLEGAL_STATE = KM_BEGIN_STATE + 1;
+
+	/*
+	 * private static final byte ATT_ID_BRAND = 0; private static final byte
+	 * ATT_ID_DEVICE = 1; private static final byte ATT_ID_PRODUCT = 2; private
+	 * static final byte ATT_ID_SERIAL = 3; private static final byte ATT_ID_IMEI =
+	 * 4; private static final byte ATT_ID_MEID = 5; private static final byte
+	 * ATT_ID_MANUFACTURER = 6; private static final byte ATT_ID_MODEL = 7;
+	 */
+  private static final short POWER_RESET_MASK_FLAG = (short) 0x4000;
+  
+  // Provider specific Commands
+  private static final byte INS_KEYMINT_PROVIDER_APDU_START = 0x00;
+  private static final byte INS_PROVISION_ATTEST_IDS_CMD = INS_KEYMINT_PROVIDER_APDU_START + 1;
+  private static final byte INS_PROVISION_PRESHARED_SECRET_CMD = INS_KEYMINT_PROVIDER_APDU_START + 2;
+  private static final byte INS_LOCK_PROVISIONING_CMD = INS_KEYMINT_PROVIDER_APDU_START + 3;
+  private static final byte INS_GET_PROVISION_STATUS_CMD = INS_KEYMINT_PROVIDER_APDU_START + 4;
+  private static final byte INS_SET_BOOT_PARAMS_CMD = INS_KEYMINT_PROVIDER_APDU_START + 5;
+  private static final byte INS_KEYMINT_PROVIDER_APDU_END = 0x1F;
+  public static final byte BOOT_KEY_MAX_SIZE = 32;
+  public static final byte BOOT_HASH_MAX_SIZE = 32;
+  
+  // Provision reporting status
+  private static final byte NOT_PROVISIONED = 0x00;
+  private static final byte PROVISION_STATUS_ATTESTATION_KEY = 0x01;
+  private static final byte PROVISION_STATUS_ATTESTATION_CERT_CHAIN = 0x02;
+  private static final byte PROVISION_STATUS_ATTESTATION_CERT_PARAMS = 0x04;
+  private static final byte PROVISION_STATUS_ATTEST_IDS = 0x08;
+  private static final byte PROVISION_STATUS_PRESHARED_SECRET = 0x10;
+  private static final byte PROVISION_STATUS_BOOT_PARAM = 0x20;
+  private static final byte PROVISION_STATUS_PROVISIONING_LOCKED = 0x40;
+  
+  public static final short SHARED_SECRET_KEY_SIZE = 32;
+  
+  private static byte keymasterState = ILLEGAL_STATE;
+  private static byte provisionStatus = NOT_PROVISIONED;
+  
+
+   
+  private boolean locked;
+  KMAndroidSEApplet() {
+    super(new KMAndroidSEProvider());
+    locked = false;
+  }
+
+  /**
+   * Installs this applet.
+   *
+   * @param bArray the array containing installation parameters
+   * @param bOffset the starting offset in bArray
+   * @param bLength the length in bytes of the parameter data in bArray
+   */
+  public static void install(byte[] bArray, short bOffset, byte bLength) {
+    //new KMAndroidSEApplet().register(bArray, (short) (bOffset + 1), bArray[bOffset]);
+	  new KMAndroidSEApplet().register();
+  }
+
+  @Override
+  public void process(APDU apdu) {
+	try {
+	    // If this is select applet apdu which is selecting this applet then return
+	    if (apdu.isISOInterindustryCLA()) {
+	      if (selectingApplet()) {
+	        return;
+	      }
+	    }
+	    short apduIns = validateApdu(apdu);
+	    if(((KMAndroidSEProvider)seProvider).isPowerReset()){
+	      super.powerReset();
+	    }
+	    
+	    if (locked) {
+	      switch (apduIns) {
+	        case INS_SET_BOOT_PARAMS_CMD:
+	          processSetBootParamsCmd(apdu);
+	          break;
+	        default:
+	          super.process(apdu);
+	          break;
+	      }
+	      return;
+	    }
+	    
+	    if(apduIns == KMType.INVALID_VALUE) return;
+	    switch (apduIns) {
+	      case INS_PROVISION_ATTEST_IDS_CMD:
+	        processProvisionAttestIdsCmd(apdu);
+	        provisionStatus |= PROVISION_STATUS_ATTEST_IDS;
+	        sendError(apdu, KMError.OK);
+	        break;
+	        
+	      case INS_PROVISION_PRESHARED_SECRET_CMD:
+	        processProvisionPreSharedSecretCmd(apdu);
+	        provisionStatus |= PROVISION_STATUS_PRESHARED_SECRET;
+	        sendError(apdu, KMError.OK);
+	        break;
+	        
+	      case INS_GET_PROVISION_STATUS_CMD:
+	        processGetProvisionStatusCmd(apdu);
+	        break;
+	        
+	      case INS_LOCK_PROVISIONING_CMD:
+	        processLockProvisioningCmd(apdu);
+	        break;
+	        
+	      case INS_SET_BOOT_PARAMS_CMD:
+			
+	        processSetBootParamsCmd(apdu);
+	        provisionStatus |= PROVISION_STATUS_BOOT_PARAM;
+	
+	        sendError(apdu, KMError.OK);
+	        break;
+	        
+	      default:
+	        super.process(apdu);
+	        break;
+	    }
+	}catch(Exception e) {
+		
+	}
+	finally {
+		repository.clean();
+	}
+  }
+  
+  private void processProvisionAttestIdsCmd(APDU apdu) {
+	  
+	  short keyparams = KMKeyParameters.exp();
+	  short cmd = KMArray.instance((short) 1);
+	  KMArray.cast(cmd).add((short) 0, keyparams);
+	  short args = receiveIncoming(apdu, cmd);
+	  	
+	  short attData = KMArray.cast(args).get((short) 0);
+	  // persist attestation Ids - if any is missing then exception occurs
+	    
+	  ((KMAndroidSEProvider)seProvider).setAttestationIds(attData);
+		/*
+		 * saveAttId((short)KMType.ATTESTATION_ID_BRAND);
+		 * saveAttId((short)KMType.ATTESTATION_ID_IMEI);
+		 * saveAttId((short)KMType.ATTESTATION_ID_DEVICE);
+		 * saveAttId((short)KMType.ATTESTATION_ID_MEID);
+		 * saveAttId((short)KMType.ATTESTATION_ID_MODEL);
+		 * saveAttId((short)KMType.ATTESTATION_ID_MANUFACTURER);
+		 * saveAttId((short)KMType.ATTESTATION_ID_PRODUCT);
+		 * saveAttId((short)KMType.ATTESTATION_ID_SERIAL);
+		 */
+  }
+  
+	/*
+	 * private void saveAttId(short attTag) { short keyParms =
+	 * KMKeyParameters.findTag(KMType.BYTES_TAG, attTag, data[KEY_PARAMETERS]); if
+	 * (keyParms != KMType.INVALID_VALUE) { keyParms =
+	 * KMByteTag.cast(keyParms).getValue();
+	 * ((KMAndroidSEProvider)seProvider).setAttestationId(mapToAttId(attTag),
+	 * KMByteBlob.cast(keyParms).getBuffer(),
+	 * KMByteBlob.cast(keyParms).getStartOff(), KMByteBlob.cast(keyParms).length());
+	 * } else { KMException.throwIt(KMError.INVALID_ARGUMENT); } }
+	 */
+
+	/*
+	 * private byte mapToAttId(short attTag) { switch (attTag) { case
+	 * KMType.ATTESTATION_ID_BRAND: return ATT_ID_BRAND; case
+	 * KMType.ATTESTATION_ID_DEVICE: return ATT_ID_DEVICE; case
+	 * KMType.ATTESTATION_ID_IMEI: return ATT_ID_IMEI; case
+	 * KMType.ATTESTATION_ID_MANUFACTURER: return ATT_ID_MANUFACTURER; case
+	 * KMType.ATTESTATION_ID_MEID: return ATT_ID_MEID; case
+	 * KMType.ATTESTATION_ID_MODEL: return ATT_ID_MODEL; case
+	 * KMType.ATTESTATION_ID_PRODUCT: return ATT_ID_PRODUCT; case
+	 * KMType.ATTESTATION_ID_SERIAL: return ATT_ID_SERIAL; }
+	 * KMException.throwIt(KMError.INVALID_TAG); return (byte) 0xFF; // should never
+	 * happen }
+	 */
+  
+  private void processProvisionPreSharedSecretCmd(APDU apdu) {
+	    
+    short blob = KMByteBlob.exp();
+    short argsProto = KMArray.instance((short) 1);
+    KMArray.cast(argsProto).add((short) 0, blob);
+    short args = receiveIncoming(apdu, argsProto);
+   
+    short val = KMArray.cast(args).get((short) 0);
+    
+    if (val != KMType.INVALID_VALUE
+        && KMByteBlob.cast(val).length() != SHARED_SECRET_KEY_SIZE) {
+      KMException.throwIt(KMError.INVALID_ARGUMENT);
+    }
+    // Persist shared Hmac.
+    ((KMAndroidSEProvider)seProvider).createPresharedKey(
+        KMByteBlob.cast(val).getBuffer(),
+        KMByteBlob.cast(val).getStartOff(),
+        KMByteBlob.cast(val).length());
+  
+  }
+
+//This function masks the error code with POWER_RESET_MASK_FLAG
+ // in case if card reset event occurred. The clients of the Applet
+ // has to extract the power reset status from the error code and
+ // process accordingly.
+ private static short buildErrorStatus(short err) {
+   short int32Ptr = KMInteger.instance((short) 4);
+   short powerResetStatus = 0;
+   if (((KMAndroidSEProvider)seProvider).isPowerReset()) {
+     powerResetStatus = POWER_RESET_MASK_FLAG;
+   }
+
+   Util.setShort(KMInteger.cast(int32Ptr).getBuffer(),
+       KMInteger.cast(int32Ptr).getStartOff(),
+       powerResetStatus);
+
+   Util.setShort(KMInteger.cast(int32Ptr).getBuffer(),
+       (short) (KMInteger.cast(int32Ptr).getStartOff() + 2),
+       err);
+
+   // reset power reset status flag to its default value.
+   //repository.restorePowerResetStatus(); //TODO
+   return int32Ptr;
+ }
+  
+  private void processGetProvisionStatusCmd(APDU apdu) {
+	  short resp = KMArray.instance((short) 2);
+	  KMArray.cast(resp).add((short) 0, buildErrorStatus(KMError.OK));
+	  KMArray.cast(resp).add((short) 1, KMInteger.uint_16(provisionStatus));
+
+	  sendOutgoing(apdu, resp);
+ }
+  
+  private void processSetBootParamsCmd(APDU apdu) {
+	short argsProto = KMArray.instance((short) 5);
+	
+	// Array of 4 expected arguments
+    // Argument 0 Boot Patch level
+    KMArray.cast(argsProto).add((short) 0, KMInteger.exp());
+    // Argument 1 Verified Boot Key
+    KMArray.cast(argsProto).add((short) 1, KMByteBlob.exp());
+    // Argument 2 Verified Boot Hash
+    KMArray.cast(argsProto).add((short) 2, KMByteBlob.exp());
+    // Argument 3 Verified Boot State
+    KMArray.cast(argsProto).add((short) 3, KMEnum.instance(KMType.VERIFIED_BOOT_STATE));
+    // Argument 4 Device Locked
+    KMArray.cast(argsProto).add((short) 4, KMEnum.instance(KMType.DEVICE_LOCKED));
+	
+    short args = receiveIncoming(apdu, argsProto);
+    
+    short bootParam = KMArray.cast(args).get((short) 0);
+    
+    ((KMAndroidSEProvider)seProvider).setBootPatchLevel(KMInteger.cast(bootParam).getBuffer(),
+												    		KMInteger.cast(bootParam).getStartOff(),
+												    		KMInteger.cast(bootParam).length());
+    
+    bootParam = KMArray.cast(args).get((short) 1);
+    if (KMByteBlob.cast(bootParam).length() > BOOT_KEY_MAX_SIZE) {
+        KMException.throwIt(KMError.INVALID_ARGUMENT);
+    } 
+    ((KMAndroidSEProvider)seProvider).setBootKey(KMByteBlob.cast(bootParam).getBuffer(),
+										            KMByteBlob.cast(bootParam).getStartOff(),
+										            KMByteBlob.cast(bootParam).length());
+    
+    bootParam = KMArray.cast(args).get((short) 2);
+    if (KMByteBlob.cast(bootParam).length() > BOOT_HASH_MAX_SIZE) {
+        KMException.throwIt(KMError.INVALID_ARGUMENT);
+    }
+    ((KMAndroidSEProvider)seProvider).setVerifiedBootHash(KMByteBlob.cast(bootParam).getBuffer(),
+												            KMByteBlob.cast(bootParam).getStartOff(),
+												            KMByteBlob.cast(bootParam).length());
+    
+    bootParam = KMArray.cast(args).get((short) 3);
+    byte enumVal = KMEnum.cast(bootParam).getVal();
+    ((KMAndroidSEProvider)seProvider).setBootState(enumVal);
+    
+    bootParam = KMArray.cast(args).get((short) 4);
+    enumVal = KMEnum.cast(bootParam).getVal();
+    ((KMAndroidSEProvider)seProvider).setDeviceLocked(enumVal == KMType.DEVICE_LOCKED_TRUE);
+ 
+    super.reboot();
+  }
+  
+  private void processLockProvisioningCmd(APDU apdu) {
+	  if (isProvisioningComplete()) {
+         // provisionStatus |= KMKeymasterApplet.PROVISION_STATUS_PROVISIONING_LOCKED;
+          //keymasterState = KMKeymasterApplet.ACTIVE_STATE;
+		  locked = true;
+		  sendError(apdu, KMError.OK);
+        } else {
+          ISOException.throwIt(ISO7816.SW_COMMAND_NOT_ALLOWED);
+        }
+  }
+  
+  @Override
+  public void onCleanup() {
+  }
+
+  @Override
+  public void onConsolidate() {
+  }
+
+  @Override
+  public void onRestore(Element element) {
+    element.initRead();
+    provisionStatus = element.readByte();
+    keymasterState = element.readByte();
+    repository.onRestore(element);
+    seProvider.onRestore(element);
+  }
+
+  @Override
+  public Element onSave() {
+    // SEProvider count
+    short primitiveCount = seProvider.getBackupPrimitiveByteCount();
+    short objectCount = seProvider.getBackupObjectCount();
+    //Repository count
+    primitiveCount += repository.getBackupPrimitiveByteCount();
+    objectCount += repository.getBackupObjectCount();
+    //KMKeymasterApplet count
+    primitiveCount += computePrimitveDataSize();
+    objectCount += computeObjectCount();
+
+    // Create element.
+    Element element = UpgradeManager.createElement(Element.TYPE_SIMPLE,
+        primitiveCount, objectCount);
+    element.write(provisionStatus);
+    element.write(keymasterState);
+    repository.onSave(element);
+    seProvider.onSave(element);
+    return element;
+  }
+
+  private short computePrimitveDataSize() {
+    // provisionStatus + keymasterState
+    return (short) 2;
+  }
+
+  private short computeObjectCount() {
+    return (short) 0;
+  }
+  
+  private short validateApdu(APDU apdu) {
+    // Read the apdu header and buffer.
+    byte[] apduBuffer = apdu.getBuffer();
+    byte apduClass = apduBuffer[ISO7816.OFFSET_CLA];
+    short P1P2 = Util.getShort(apduBuffer, ISO7816.OFFSET_P1);
+
+    // Validate APDU Header.
+    if ((apduClass != CLA_ISO7816_NO_SM_NO_CHAN)) {
+      sendError(apdu, KMError.UNSUPPORTED_CLA);
+      return KMType.INVALID_VALUE;
+    }
+
+    // Validate P1P2.
+    if (P1P2 != KMKeymasterApplet.KM_HAL_VERSION) {
+      sendError(apdu, KMError.INVALID_P1P2);
+      return KMType.INVALID_VALUE;
+    }
+    return apduBuffer[ISO7816.OFFSET_INS];
+  }
+  
+  private boolean isProvisioningComplete() {
+	    if ((0 != (provisionStatus & PROVISION_STATUS_ATTEST_IDS))
+	    	&& (0 != (provisionStatus & PROVISION_STATUS_BOOT_PARAM))	
+	        && (0 != (provisionStatus & PROVISION_STATUS_PRESHARED_SECRET))) {
+	      return true;
+	    } else {
+	      return false;
+	    }
+  }
+}
+
